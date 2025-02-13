577636e96039f7cd9c35f1771464c9a17528764e
maxduran08 <2997082920101@ingenieria.usac.edu.gt>
2025-02-12 20:00:36 -0600

    [ADD] Se agrego material de la clase de 12-02-2025

---
+++ Practica 1/Ejemplo/practica/src/practica/Ejemplo_12_02_2024.java
@@ -0,0 +1,44 @@
+package practica;
+
+public class Ejemplo_12_02_2024 {
+
+    public static void main(String[] args) {
+
+        // Expresiones aritméticas
+        int num1 = 1000;
+        int num2 = 30;
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        int multiplicacion = num1 * num2;
+        double division = (num2 != 0) ? (double) num1 / num2 : 0;
+        //operador ternario (? :)
+        //(Condicion) ? True : False;
+        
+        int residuo = 10 % 2;
+        /*
+        Explicación:
+        La operación 10 % 2 se calcula así:
+        Se divide 10 / 2, lo que da 5 (división entera).
+        Se multiplica 5 * 2 = 10.
+        Se resta 10 - 10 = 0 (este es el residuo).
+        Por lo tanto, 10 % 2 devuelve 0.
+        */
+        
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicacion: " + multiplicacion);
+        System.out.println("Division: " + division);
+        System.out.println("Residuo: " + residuo);
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicacion: " + multiplicacion);
+        System.out.println("Division: " + division);
+        System.out.println("Residuo: " + residuo);
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicacion: " + multiplicacion);
+        System.out.println("Division: " + division);
+        System.out.println("Residuo: " + residuo);
+    }
+    
+}
